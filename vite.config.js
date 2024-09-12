9297d71e9acda38f4b64e2c25cd16546be5ac4c4
Coders Club <hi@codersclub.com>
2024-09-12 23:10:59 +0300

    vite.config güncellendi

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  build: {
+    rollupOptions: {
+      external: [] // Boş bırakmak harici modüller sorunu çözebilir
+    }
+  }
 })
-
