a8b9b8ab3206829d045954e3bfa633d2672ccad7
Coders Club <hi@codersclub.com>
2024-09-14 21:59:49 +0300

    Sepete ekleme kısımları ve sepet kısımları düzeltildi

---
+++ src/Api/Reducer.js
@@ -0,0 +1,55 @@
+export const initialState = {
+    query: "",
+    categorys: "",
+    favorites: localStorage.getItem("favorites") ? JSON.parse(localStorage.getItem("favorites")) : [],
+    basket: localStorage.getItem("basket") ? JSON.parse(localStorage.getItem("basket")) : [],
+  };
+  export function ProductReducer(state, action) {
+    switch (action.type) {
+      case "ADD_FAVORITE":  // Ürünü favorilere ekleme
+        return { ...state, favorites: [...state.favorites, action.product] };
+  
+      case "REMOTE_FAVORITE": // Ürünü Favorilerden çıkarma
+        return { ...state, favorites: state.favorites.filter(item => item.id !== action.product.id) };
+  
+      case "ADD_BASKET": // Sepete ürün ekleme, aynı üründen sepette varsa adet arttırma
+      const existingProduct = state.basket.find(item => item.id === action.product.id);
+        if (existingProduct) {
+          return {
+            ...state,
+            basket: state.basket.map(item =>
+              item.id === action.product.id ? { ...item, quantity: item.quantity + (action.product.quantity || 1) } : item
+            )
+          };
+        } else {
+          return {
+            ...state,
+            basket: [...state.basket, { ...action.product,  quantity: action.product.quantity || 1 }]
+          };
+        }
+  
+      case "REMOVE_BASKET":  // Sepetten ürünü silme
+        return {
+          ...state,
+          basket: state.basket.filter(item => item.id !== action.product.id)
+        };
+  
+        case "UPDATE_QUANTITY":  // Ürün adedi bilgisini güncelleme
+        return {
+          ...state,
+          basket: state.basket.map(item =>
+            item.id === action.product.id ? { ...item, quantity:action.quantity } : item
+          )
+        };
+  
+      case "QUERY": // Ürün filtreleme input ile
+        return { ...state, query: action.payload };
+  
+      case "CATEGORY": // Kategori filtreleme select ile
+        return { ...state, categorys: action.payload };
+  
+      default:
+        return state;
+    }
+  }
+  
--- src/pages/UrunKatalog.jsx
+++ src/pages/UrunKatalog.jsx
@@ -6,7 +6,7 @@ import { Productcontext } from "../context/ProductContext";
 import { FaSearch } from 'react-icons/fa';
 
 function UrunKatalog() {
-  const { AddFavorite,isFavorite,addBasket,updateQuantity,HandleSearch,query} = useContext(Productcontext);
+  const { AddFavorite,isFavorite,addBasket,HandleSearch,query,HandleSelect,categorys} = useContext(Productcontext);
 
   const navigate = useNavigate();
   const { data, error, isLoading } = useProducts();
@@ -14,12 +14,14 @@ function UrunKatalog() {
   const navigateProduct = (id) => {
     navigate(`/UrunDetay/${id}`);
   };
-
-
   const filteredProducts = data ? data.filter(product =>
-    product.title.toLowerCase().includes(query.toLowerCase()) ||
-    product.category.toLowerCase().includes(query.toLowerCase())
+ 
+    (product.title.toLowerCase().includes(query.toLowerCase()) ||
+    product.category.toLowerCase().includes(query.toLowerCase())) &&
+    (categorys === "" || categorys === "All Categories" || product.category.toLowerCase() === categorys.toLowerCase())
   ) : [];
+
+
   
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error loading products</div>;
@@ -27,8 +29,18 @@ function UrunKatalog() {
     <div className="flex flex-col ">
      
   
-     <div className="relative flex justify-center items-center p-2">
+     <div className="relative flex justify-center items-center p-2 bg-slate-400">
+     <select onChange={HandleSelect} value={categorys} className="m-4 border-2 border-collapse rounded-sm w-48">
+     <option value="All Categories">All Categories</option>
+   
+    <option value="men's clothing">men's clothing</option>
+    <option value="Women's clothing">Women's clothing</option>
+    <option value="Electronics">Electronics</option>
+
+
+    </select>
   <div className="relative w-80">
+ 
     <input
       onChange={HandleSearch}
       type="text"
@@ -37,11 +49,13 @@ function UrunKatalog() {
       placeholder="Ürün,Marka veya kategori arayınız"
      
     />
+ 
     <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
   </div>
+  
 </div>
 
-      <div className="p-4 ">
+      <div className="px-4">
         {filteredProducts.map((product) => (
           <div
             className="relative flex border-solid border-2 rounded-md m-2"
@@ -62,7 +76,7 @@ function UrunKatalog() {
 
                   </button>
             </div>
-            <div className="text-[18px] p-3 m-5">
+            <div className="text-[16px] p-3 m-5">
               <p className="font-semibold">
                 {" "}
                 <span className="font-semibold text-red-500">
@@ -80,24 +94,19 @@ function UrunKatalog() {
                 <span className="font-semibold text-red-500">Kategori: </span>{" "}
                 {product.category}
               </p>
-              <p className="font-semibold">
-                <span className="font-semibold text-red-500">Adet: </span>
-               
-                <input type='number' min="1" onChange={(e) => updateQuantity(product.id, e.target.value)}
-                 className='w-12 h-[30px] p-1 text-center border border-gray-300' value={product.quantity}></input> 
-              </p>
+           
             </div>
 
-            <div className="flex  ml-auto justify-center items-center text-center ">
+            <div className="flex ml-auto justify-center items-center text-center ">
               <button
-                className="bg-red-400 p-2 m-3  border-solid border-1 rounded-md hover:bg-red-300"
+                className="bg-red-400 p-2 m-3  border-solid  w-auto  border-1 rounded-md hover:bg-red-300"
                 onClick={() => navigateProduct(product.id)}
               >
                 Ürün Detay{" "}
               </button>
               <button
                 className="bg-green-400 p-2 m-3  border-solid border-1 rounded-md hover:bg-green-300"
-                onClick={() => addBasket(product)}
+                onClick={() => addBasket(product,product.quantity || 1)}
               >
                Sepete Ekle
               </button>
