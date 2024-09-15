b7d3d83a5c4dcc38347554b99ce070053e07abf4
Coders Club <hi@codersclub.com>
2024-09-15 17:33:07 +0300

    Header kısmı Mobil görünüm düzeltildi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ import { Productcontext } from "../context/ProductContext";
 function Header() {
 const {basket}=useContext(Productcontext)
   return (
-    <div className="bg-pink-800 w-screen  h-[50px] flex items-center justify-between px-4">
+    <div className="bg-pink-800 w-full h-[50px] flex items-center justify-between px-4">
      <div className="flex-grow flex justify-center font-medium text-zinc-50">
-      <ul className="flex  space-x-6 ">
+      <ul className="flex  space-x-4 ">
         <li >
           <Link className="no-underline  hover:text-gray-400" to="/">
             Anasayfa
@@ -31,9 +31,9 @@ const {basket}=useContext(Productcontext)
        
       </ul>
 </div>
-      <div className="flex justify-between mx-3 text-3xl text-white">
+      <div className="flex items-center text-3xl text-white">
        
-      <div className="text-xl text-white flex text-center justify-center items-center ">{basket.length}</div> 
+      <div className="text-xl text-white flex text-center justify-center">{basket.length}</div> 
           <Link className="no-underline  hover:text-gray-400" to="/Sepet">
           
            <FiShoppingCart  />
